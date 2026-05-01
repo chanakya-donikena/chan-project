2cf58513a2e02a73d36cecb32136224b0b2b92e3
Chanakya D <chanakya.donikena@kitaboo.com>
2026-05-01 20:52:34 +0530

    updated terraform manifests

---
+++ terraform-manifests/02-variables.tf
@@ -0,0 +1,11 @@
+variable "chan_rg_name" {
+  description = "This is the resource group name"
+  type = string
+  default = "kml_rg_main-3434de88bd154e65"
+}
+
+variable "chan_rg_location" {
+  description = "This is the Resource Group Location"
+  type = string
+  default = "eastus"
+}
